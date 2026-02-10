7b9b07145fbea3eeccb18b92d10d1dd4e3906ee5
Baker <baker@shamlan.sa>
2026-02-10 04:50:18 +0300

    Updates

--- _nuxt/BR0FJYuD.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./BB335ivP.js";import t from"./Jeuhg-ZO.js";import{X as e,a as n,l as s,q as c,o as i}from"./D_7-oAm_.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(i(),n(t,{color:"error",icon:c(o).ui.icons.caution},{default:s(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
